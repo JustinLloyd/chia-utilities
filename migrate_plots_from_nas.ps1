a295485be1b4e98fdaa5ae7f3f39ed45bb90a2c9
Justin Lloyd <justin@justinlloyd.io>
2021-04-21 01:37:36 -0700

    Changed fro robocopy technique to BITS Transfer technique

--- migrate_plots_from_nas.ps1
+++ migrate_plots_from_nas.ps1
@@ -1,5 +1,6 @@
 ﻿# Description: This file copies the plots from the NAS to the farming drives
-$folders=@('\\s-lloyd-02\chia-01\*.plot')
+$Config = Import-PowershellDataFile -Path .\config.psd1
+$IntermediatePath = $Config.IntermediatePath
 # clean up .farmtmp files on destination drives
 # change folders to be source folders 
 # add destination folders list
@@ -8,28 +9,49 @@ $folders=@('\\s-lloyd-02\chia-01\*.plot')
 # read settings from config.yaml
 # verify only one instance is running at a time (PID check or get-process)
 # reprocess robocopy output to make a nicer progress bar and cleaner output
-foreach ($folder in $folders)
+# get a list of all plots files on the NAS sorted by size in descending order, so the biggest files are listed first
+$PlotFilesToMove = Get-ChildItem -Path $IntermediatePath -Filter '*.plot' -ErrorAction Ignore `
+    | Sort -Descending -Property Length
+foreach ($PlotFile in $PlotFilesToMove)
 {
-    $files=dir $folder | sort -descending -property length
-    foreach ($biggestfile in $files)
+    echo $PlotFile
+    # FInd a spinning rust hard drive with the least amount of space capable of storing the largest plot file
+    $TargetDrive = Get-PhysicalDisk | Where -Property MediaType -eq -Value HDD `
+        | Get-Disk | Get-Partition | Get-Volume `
+        | Sort -Property SizeRemaining `
+        | Where -Property SizeRemaining -ge -Value $PlotFilesToMove.Length `
+        | Select -First 1
+    # TODO add error checking to ensure we have a drive capable of storing our plot file
+    # if we cannot store the plot file, what do we want to do? throw an error? skip the plot file?
+#    echo $targetdrive
+
+    $DestinationPath = $TargetDrive.DriveLetter + ':\'
+    try
     {
-        echo $biggestfile
-        $targetdrive=get-physicaldisk | where -property mediatype -eq -value HDD |  Get-Disk | Get-Partition | Get-Volume | sort -Property sizeremaining | where -property sizeremaining -ge -value $biggestfile.length | select -first 1
-    #    echo $targetdrive
-
-        $destinationpath = $targetdrive.DriveLetter + ':\'
-    #    echo $destinationpath
-        # rename the file to have a .farmtmp extension, move the file using robocopy to the final resting place, remove the .farmtmp extension
-        $TempName = "$($biggestfile.Name).farmtmp"
-        echo "TempName", $TempName
-        $TempSrcFilepath = Join-Path $biggestfile.DirectoryName $TempName
-        $TempDestFilepath = Join-Path $destinationpath $TempName
-        echo "TempSrcFilePath", $TempSrcFilepath
-        echo "TempDestFilePath", $TempDestFilepath
-        Rename-Item $biggestfile.FullName $TempSrcFilepath
-        robocopy /mt /j /mov $biggestfile.DirectoryName $destinationpath $TempName
-        Rename-Item $TempDestFilepath (Join-Path $destinationpath $biggestfile.Name)
-
-    # move-item -path $biggestfile -destination $destinationpath -verbose
+        Start-BitsTransfer -Source $PlotFile -Destination $DestinationPath `
+            -DisplayName 'Migrate plots from NAS' `
+            -Description ('Migrating plot (' + $count + ' of ' + $PlotFilesToMove.Count + ') "' + $file.Name + '" to "' + $DestinationPath + '"') `
+            -ErrorAction Stop
+        Remove-Item $PlotFile -Confirm
     }
+
+    catch
+    {
+        echo "Transfer process was interrupted or failed due to network error"
+        exit
+    }
+
+#    echo $destinationpath
+    # rename the file to have a .farmtmp extension, move the file using robocopy to the final resting place, remove the .farmtmp extension
+    #$TempName = "$($biggestfile.Name).farmtmp"
+    #echo "TempName", $TempName
+    #$TempSrcFilepath = Join-Path $biggestfile.DirectoryName $TempName
+    #$TempDestFilepath = Join-Path $destinationpath $TempName
+    #echo "TempSrcFilePath", $TempSrcFilepath
+    #echo "TempDestFilePath", $TempDestFilepath
+    #Rename-Item $biggestfile.FullName $TempSrcFilepath
+    #robocopy /mt /j /mov $biggestfile.DirectoryName $destinationpath $TempName
+    #Rename-Item $TempDestFilepath (Join-Path $destinationpath $biggestfile.Name)
+
+# move-item -path $biggestfile -destination $destinationpath -verbose
 }
