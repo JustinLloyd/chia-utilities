a00e747679e29eae4163a3cc3e94a8579749ad5d
Justin Lloyd <justin@justinlloyd.io>
2021-04-21 12:24:23 -0700

    Added a requirement for Powershell 7 which fixes the issue with BITS transfer not using the proper path from the  variable. Removed the confirmation on plot delete after migration now we are sure that part of the code is working.

--- migrate_plots_from_nas.ps1
+++ migrate_plots_from_nas.ps1
@@ -1,9 +1,11 @@
-﻿# Description: This file copies the plots from the NAS to the farming drives
-$Config = Import-PowershellDataFile -Path .\config.psd1
+﻿#Requires -Version 7.0
+
+# Description: This file copies the plots from the NAS to the farming drives
+$Config = Import-PowershellDataFile -Path .\config.psd1 -ErrorAction Stop
 $IntermediatePath = $Config.IntermediatePath
-# TODO change folders to be source folders 
+# TODO change destination folders to be source folders 
 # TODO add destination folders list
-# TODO add a switch to toggle between HDDs and specified desintation folders
+# TODO add a switch to toggle between any and all HDDs and specified destination folders
 # TODO verify only one instance is running at a time (PID check or get-process)
 
 # get a list of all plots files on the NAS sorted by size in descending order, so the biggest files are listed first
@@ -13,7 +15,7 @@ echo "Found $($PlotFilesToMove.Count) plot files that need to be migrated."
 $FileIndex = 0
 foreach ($PlotFile in $PlotFilesToMove)
 {
-    $FileIndex=$FileIndex+1
+    $FileIndex = $FileIndex + 1
     # Find a spinning rust hard drive with the least amount of space capable of storing the largest plot file
     $TargetDrive = Get-PhysicalDisk | Where -Property MediaType -eq -Value HDD | Get-Disk | Get-Partition | Get-Volume | Sort -Property SizeRemaining | Where -Property SizeRemaining -ge -Value $PlotFile.Length | Select -First 1
     # TODO add error checking to ensure we have a drive capable of storing our plot file
@@ -22,11 +24,11 @@ foreach ($PlotFile in $PlotFilesToMove)
     echo ('Migrating plot (' + $FileIndex + ' of ' + $PlotFilesToMove.Count + ') "' + $PlotFile.Name + '" to "' + $DestinationPath + '"')
     try
     {
-        Start-BitsTransfer -Source $PlotFile.FullName -Destination $DestinationPath `
+        Start-BitsTransfer -Source $PlotFile -Destination $DestinationPath `
             -DisplayName 'Migrate plots from NAS' `
             -Description ('Migrating plot (' + $FileIndex + ' of ' + $PlotFilesToMove.Count + ') "' + $PlotFile.Name + '" to "' + $DestinationPath + '"') `
             -ErrorAction Stop
-        Remove-Item $PlotFile -Confirm
+        Remove-Item $PlotFile
     }
 
     catch
