3720e6f43ec88f848544a23825718b61c81eb890
Justin Lloyd <justin@justinlloyd.io>
2021-05-03 19:04:07 -0700

    Add some error checking for when plots are migrated to the NAS

--- migrate_plots_to_nas.ps1
+++ migrate_plots_to_nas.ps1
@@ -25,6 +25,19 @@ foreach ($PlotFile in $PlotFilesToMove)
     try
     {
         Start-BitsTransfer -Source $PlotFile -Destination $IntermediatePath -DisplayName 'Migrate plots to NAS' -Description ('Migrating plot (' + $FileIndex + ' of ' + $PlotFilesToMove.Count + ') "' + $PlotFile.Name + '" to "' + $IntermediatePath + '"') -ErrorAction Stop
+        $MovedPlotFile = Get-ChildItem -Path (Join-Path $IntermediatePath $PlotFile.Name) -File -ErrorAction Ignore
+        if ($MovedPlotFile -eq $null)
+        {
+            echo "Failed to correctly move file $($PlotFile.FullName). It doesn't exist on the destination path at $($IntermediatePath). Double-check everything and try again. Aborting so you don't lose any plots."
+            exit
+        }
+        if ($MovePlotFile.Length -ne $PlotFile.Length)
+        {
+            echo "Failed to correctly move file $($PlotFile.Name). The size of the plot file at the destination path $(Join-Path $IntermediatePath $PlotFile.Name) differs from the size of file at $($PlotFile.Fullname). Double-check everything and try again. Aborting so you don't lose any plots."
+            Remove-Item (Join-Path $IntermediatePath $PlotFile.Name) -ErrorAction Ignore
+            exit
+        }
+
         Remove-Item $PlotFile
     }
 
