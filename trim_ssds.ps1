fca29f42242401002f8ad4a2166e7097def9182b
Justin Lloyd <justin@justinlloyd.io>
2021-05-25 01:04:20 -0700

    Add a note about needing admin access to trim SSDs

--- trim_ssds.ps1
+++ trim_ssds.ps1
@@ -1,5 +1,6 @@
 #Requires -Version 7.0
 
 # Description: Performs a trim command on all of the SSDs attached to the system
+# NOTE: This needs to run from an elevated PowerShell
 
 Get-PhysicalDisk | Where -Property MediaType -eq -Value SSD | Get-Disk | Get-Partition -ErrorAction Ignore | Get-Volume | Optimize-Volume -Retrim -ErrorAction Ignore
