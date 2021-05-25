f32a3140b15b0e807e0b58f0b0a18a2e3ff79d19
Justin Lloyd <justin@justinlloyd.io>
2021-05-25 01:03:08 -0700

    Trim the SSDs attached to the system

---
+++ trim_ssds.ps1
@@ -0,0 +1,5 @@
+#Requires -Version 7.0
+
+# Description: Performs a trim command on all of the SSDs attached to the system
+
+Get-PhysicalDisk | Where -Property MediaType -eq -Value SSD | Get-Disk | Get-Partition -ErrorAction Ignore | Get-Volume | Optimize-Volume -Retrim -ErrorAction Ignore
