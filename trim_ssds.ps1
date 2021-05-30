cf625ec53abd4decd82a146f57bf4dc6993481fb
Justin Lloyd <justin@justinlloyd.io>
2021-05-29 18:35:02 -0700

    Wrap the SSD trim operation in a simple function

--- trim_ssds.ps1
+++ trim_ssds.ps1
@@ -3,4 +3,7 @@
 # Description: Performs a trim command on all of the SSDs attached to the system
 # NOTE: This needs to run from an elevated PowerShell
 
-Get-PhysicalDisk | Where -Property MediaType -eq -Value SSD | Get-Disk | Get-Partition -ErrorAction Ignore | Get-Volume | Optimize-Volume -Retrim -ErrorAction Ignore
+Function Trim-SSDs
+{
+    Get-PhysicalDisk | Where -Property MediaType -eq -Value SSD | Get-Disk | Get-Partition -ErrorAction Ignore | Get-Volume | Optimize-Volume -Retrim -ErrorAction Ignore
+}
